9fa4411d95a23058ffc77770351b5ba5193f7189
adichopra11 <adi.chopra108@gmail.com>
2021-03-09 18:24:12 +0530

    Vectorizing Perceptron Implementation

--- perceptron/Perceptron.py
+++ perceptron/Perceptron.py
@@ -8,47 +8,58 @@ from mpl_toolkits.mplot3d import Axes3D
 
 from utils import get_weight
 
+# TODO: Add other Performance Metrix:
+#   - Accuracy
+#   - Recall
+#   - Precision
+#   - F-Score
+# 
+# TODO: Add Decision Boundary 
+
 class Perceptron:
     def __init__(self, num_features, initialization='Random') -> None:
         self.num_features = num_features
         self.w = get_weight((num_features, 1, initialization))
 
-    def classify(self, x):
-        return 1 if self.w.T @ x > 0 else -1
+    def classify(self, X):
+        y = X @ self.w
+        y[y > 0] = 1
+        y[y < 0] = -1
+
+        return y
 
     def get_misclassified(self, X, y):
-        misclassified = []
-        for x, t in zip(X, y):
-            result = t * self.classify(x)
-            if result < 0:
-                misclassified.append((x, t))
+        y_hat = self.classify(X).squeeze()
 
-        return misclassified
+        mask = y != y_hat 
+
+        return X[mask], y[mask]
 
     def sgd(self, X, y, learning_rate=1, epochs=5, print_freq=1):
 
         accuracy = []
 
         for i in tqdm(range(epochs)):
-            misclassified = self.get_misclassified(X, y)
+            misclassified_X, misclassified_y = self.get_misclassified(X, y)
 
-            acc = 1 - len(misclassified) / X.shape[0]
+            acc = 1 - len(misclassified_X) / X.shape[0]
             accuracy.append(acc)
 
             if print_freq and i % print_freq == 0:
                 print(f"Accuracy at Iteration[{i}]: {acc}")
 
-            # for x, t in tqdm(misclassified):
-            for x, t in misclassified:
-            
-                self.w += learning_rate * t * x.reshape(-1, 1)
+            self.w += (misclassified_X * misclassified_y.reshape(-1, 1)).sum(axis=0).T.reshape(-1, 1)
 
         return accuracy
 
     def test(self, X, y):
-        misclassified = self.get_misclassified(X, y)
+        misclassified_X, _ = self.get_misclassified(X, y) # Ignoring Misclassifed y
 
-        acc = 1 - len(misclassified) / X.shape[0]
+        acc = 1 - len(misclassified_X) / X.shape[0]
         
         return acc
+
+
+    def performance_metrics(self, X, y):
+        pass
 
