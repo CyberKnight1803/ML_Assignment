cf190c9a7e394be8d9451f2ebd2c5f83702412f0
adichopra11 <adi.chopra108@gmail.com>
2021-03-11 23:13:04 +0530

    Using New Performance Metrics

--- perceptron/main.py
+++ perceptron/main.py
@@ -1,44 +1,76 @@
 import matplotlib.pyplot as plt
 
+import pandas as pd
+
 from Perceptron import Perceptron
 from utils import load_dataset, train_test_split
+
+MAX_EPOCHS = int(1.5e5)
+
 
 def find_best_lr(training, testing):
     learning_rates = [0.125, 0.25, 0.45, 0.5, 0.75, 0.85, 0.9, 0.99, 1]
 
     accuracies = {}
 
+    df = pd.DataFrame(
+        columns=["lr", "Iterations", "Accuracy", "Precision", "Recall", "F1-Score"])
+
     for lr in learning_rates:
         perceptron = Perceptron(X.shape[1])
-        accuracy = perceptron.sgd(training["X"], training["y"], learning_rate=0.45, epochs=int(1e6), print_freq=0)
+        accuracy = perceptron.fit(
+            training["X"], training["y"], learning_rate=0.45, epochs=MAX_EPOCHS, print_freq=0)
         accuracies[lr] = accuracy
 
-        testing_acc = perceptron.test(testing["X"], testing["y"])
-        print(f"[lr: {lr}] Tesiting Set Accuracy: {testing_acc}")
+        metrics = perceptron.test(testing["X"], testing["y"])
 
-    
+        df.loc[-1] = [lr, metrics["iterations"], metrics['accuracy'],
+                      metrics['precision'],
+                      metrics['recall'],
+                      metrics['f1-score']]
+
+        df.index += 1
+
+    df = df.sort_index()
+    df.to_clipboard()
+
+    print("\nResults...")
+    print(df)
+
     for lr, accuracy in accuracies.items():
         plt.plot(accuracy)
-        
-        
+
     plt.legend(learning_rates)
     plt.show()
 
 
 def ez(training, testing):
+    learning_rate = 0.45
     perceptron = Perceptron(X.shape[1])
-    accuracy = perceptron.sgd(
-        training["X"], training["y"], learning_rate=0.45, epochs=100, print_freq=0)   
+    accuracy = perceptron.fit(
+        training["X"], training["y"], learning_rate=learning_rate, epochs=MAX_EPOCHS, print_freq=0)
 
     plt.plot(accuracy)
     plt.show()
 
-    testing_acc = perceptron.test(testing["X"], testing["y"])
-    print(f"Accuracy on Testing Set: {testing_acc}")
+    df = pd.DataFrame(
+        columns=["lr", "Accuracy", "Precision", "Recall", "F1-Score"])
+
+    metrics = perceptron.test(testing["X"], testing["y"])
+
+    df.loc[-1] = [learning_rate, metrics['accuracy'],
+                  metrics['precision'],
+                  metrics['recall'],
+                  metrics['f1-score']]
+
+    df.index += 1
+
+    print(df)
+
 
 if __name__ == '__main__':
-    path = 'dataset_LP_1.txt'
-    # path = 'dataset_LP_2.csv'
+    # path = 'dataset_LP_1.txt'
+    path = 'dataset_LP_2.csv'
 
     X, y = load_dataset(path)
 
@@ -51,4 +83,4 @@ if __name__ == '__main__':
     print(training["X"].shape, training["y"].shape,
           testing["X"].shape, testing["y"].shape)
 
-    ez(training, testing)
+    find_best_lr(training, testing)
