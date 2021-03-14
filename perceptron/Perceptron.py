65c17b3718e1b469bad69b822ea5e84d1cbcef9d
adichopra11 <adi.chopra108@gmail.com>
2021-03-14 23:19:07 +0530

    Added SGD Instead of BGD

--- perceptron/Perceptron.py
+++ perceptron/Perceptron.py
@@ -1,9 +1,10 @@
 from tqdm import tqdm
 from collections import Counter
+import random
 
 from utils import get_weight
 
-# TODO: Add checks for division by zero 
+# TODO: Add checks for division by zero
 
 MAX_ITERATIONS = int(1e6)
 
@@ -27,7 +28,10 @@ class Perceptron:
 
         return X[mask], y[mask]
 
-    def fit(self, X, y, learning_rate=1, epochs=5, print_freq=1):
+    def fit(self, X, y, learning_rate=1, epochs=5, print_freq=1, batch=True):
+
+        if not batch:
+            return self.sgd(X, y, learning_rate=learning_rate, iterations=epochs * X.shape[0], print_freq=print_freq)
 
         accuracy = []
 
@@ -62,6 +66,30 @@ class Perceptron:
         print(f"Trained for {self.iterations} iterations")
         return accuracy
 
+    def sgd(self, X, y, learning_rate=1, iterations=1000000, print_freq=0):
+
+        accuracy = []
+        for i in tqdm(range(iterations)):
+            misclassified_X, misclassified_y = self.get_misclassified(X, y)
+
+            acc = 1 - misclassified_X.shape[0] / X.shape[0]
+            accuracy.append(acc)
+
+            if acc == 1:
+                return accuracy
+
+            if print_freq and i % print_freq == 0:
+                print(f"Accuracy at Iteration[{i}]: {acc}")
+
+            idx = random.randrange(0, misclassified_X.shape[0])
+            self.w += (learning_rate *
+                       misclassified_X[idx] * misclassified_y[idx]).reshape(-1, 1)
+
+        self.iterations = iterations
+
+        print(f"Trained for {self.iterations} iterations")
+        return accuracy
+
     def test(self, X, y):
         y_hat = self.classify(X).squeeze()
 
@@ -79,7 +107,7 @@ class Perceptron:
 
         metrics["iterations"] = self.iterations
         metrics["accuracy"] = (tp + tn) / X.shape[0]
-        metrics["recall"] = tp / (tp  + fn)
+        metrics["recall"] = tp / (tp + fn)
         metrics["precision"] = tp / (tp + fp)
         metrics["f1-score"] = (2 * tp) / (2 * tp + fp + fn)
 
